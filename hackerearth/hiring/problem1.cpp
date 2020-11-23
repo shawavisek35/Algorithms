c2d158e362e55422c3d35827874b108a29702087
shawavisek35 <shawavisek35@gmail.com>
2020-11-23 21:49:58 +0530

    new Problems addwd

---
+++ hackerearth/hiring/problem1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for(int i=0;i<t;i++)
+    {
+        solve();
+    }
+    return 0;
+}
