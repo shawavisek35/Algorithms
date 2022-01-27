43b72f6e0b4db658e3b142bba6bf06cc1d488cb9
shawavisek35 <shawavisek35@gmail.com>
2022-01-27 18:16:25 +0530

    tree ++

---
+++ trees/bottomLeftValueOfTree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        int ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty()) {
+            int size = q.size();
+            for(int i=0;i<size;i++) {
+                TreeNode *temp = q.front();
+                q.pop();
+                if(i==0) ans = temp->val;
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+        }
+        return ans;
+    }
+};
---
+++ trees/leftViewOfTree.cpp
@@ -0,0 +1,18 @@
+vector<int> leftView(Node *root)
+{
+   vector<int> ans;
+   if(!root) return ans;
+   queue<Node *> q;
+   q.push(root);
+   while(!q.empty()) {
+       int size = q.size();
+       for(int i=0;i<size;i++) {
+           Node *temp = q.front();
+           q.pop();
+           if(i==0) ans.push_back(temp->data);
+           if(temp->left) q.push(temp->left);
+           if(temp->right) q.push(temp->right);
+       }
+   }
+   return ans;
+}
