3dcf68555630e333e3a547ffaa56eb58c69ba79a
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-02-16 17:59:18 +0530

    practice

---
+++ trees/leetcodePractice/rangeSumInBst.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void dfs(TreeNode *root, int low, int high, int &ans) {
+        if(!root) return;
+        if(root->val > high) {
+            return dfs(root->left, low, high, ans);
+        }
+        else if(root->val < low) {
+            return dfs(root->right, low, high, ans);
+        }
+        else {
+            ans += root->val;
+            dfs(root->left, low, high, ans);
+            dfs(root->right, low, high, ans);
+            return;
+        }
+        
+    }
+    
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int ans = 0;
+        dfs(root, low, high, ans);
+        return ans;
+    }
+};
