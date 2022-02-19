e83d7ecd64a63662588049454ec10d7d39766ed5
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-02-19 23:11:22 +0530

    Create populateNextRightPointer.cpp

---
+++ populateNextRightPointer.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node *> q;
+        if(!root) return root;
+        q.push(root);
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i=0;i<n;i++) {
+                Node *temp = q.front();
+                q.pop();
+                if(i==n-1) {
+                    temp->next = NULL;
+                }
+                else {
+                    temp->next = q.front();
+                }
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+        }
+        return root;
+    }
+};
