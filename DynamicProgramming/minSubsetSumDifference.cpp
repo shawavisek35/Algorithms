ed2c4c7d1d4b1afbff5106410128fab068c23916
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-03-06 10:29:40 +0530

    Update minSubsetSumDifference.cpp

--- DynamicProgramming/minSubsetSumDifference.cpp
+++ DynamicProgramming/minSubsetSumDifference.cpp
@@ -27,3 +27,36 @@ int minSubsetSumDifference(vector<int>& arr, int n)
     }
     return ans;
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//space optimised
+int minSubsetSumDifference(vector<int>& arr, int n)
+{
+	int sum = 0;
+    for(auto it : arr) sum += it;
+    int dp[2][sum+1];
+    for(int i=0;i<=n;i++) {
+        for(int j=0;j<=sum;j++) {
+            if(j==0) dp[i%2][j] = true;
+            else if(i==0) dp[i%2][j] = false;
+            else {
+                if(arr[i-1] > j) {
+                    dp[i%2][j] = dp[(i+1)%2][j];
+                }
+                else {
+                    dp[i%2][j] = dp[(i+1)%2][j] || dp[(i+1)%2][j-arr[i-1]];
+                }
+            }
+        }
+    }
+    
+    //finding minsum;
+    int ans = INT_MAX;
+    for(int i=0;i<=sum; i++) {
+        if(dp[n%2][i]) ans = min(ans, abs(sum - 2*i));
+    }
+    return ans;
+}
+
