500d5a6d1a9c0974dd27641d0ab911b213ab1708
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-03-08 20:40:05 +0530

    Create countSubsetSum.cpp

---
+++ DynamicProgramming/countSubsetSum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countSum(vector<int> &nums, int n, int target, vector<vector<int>> &dp) {
+    if(target==0) return 1;
+    if(dp[n][target] != -1) return dp[n][target];
+    if(n==0 || target<0) {
+        return 0;
+    }
+    
+    if(nums[n-1] > target) {
+        return countSum(nums, n-1, target, dp);
+    }
+    return dp[n][target] = countSum(nums, n-1, target, dp) + countSum(nums, n-1, target - nums[n-1], dp);
+}
+
+int findWays(vector<int> &nums, int tar)
+{
+    
+    int n = nums.size();
+    vector<vector<int>> dp(n+1, vector<int>(tar+1, -1));
+	return countSum(nums, n, tar, dp);
+}
