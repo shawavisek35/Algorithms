492720d384f66f8c997262f0a35e42a517bf2eff
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-03-10 07:22:57 +0530

    Update countSubsetSum.cpp

--- DynamicProgramming/countSubsetSum.cpp
+++ DynamicProgramming/countSubsetSum.cpp
@@ -1,10 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//if the input array contains 0 as well
-//then the result will be 
-//let n be the total no. of zeros present
-//result = poy(2, n) * ans;
+//if 1<=arr[i]<=n;
 
 int countSum(vector<int> &nums, int n, int target, vector<vector<int>> &dp) {
     if(target==0) return 1;
@@ -25,4 +22,17 @@ int findWays(vector<int> &nums, int tar)
     int n = nums.size();
     vector<vector<int>> dp(n+1, vector<int>(tar+1, -1));
     return countSum(nums, n, tar, dp);
+}
+
+////if 0<=arr[i]<=n;
+int subsetSum(vector<int> &arr, int n, int target, vector<vector<int>> &dp) {
+    if(n == 1){
+        if(target == 0 && arr[0] == 0) return 2;
+        if(target == 0 || target == arr[0]) return 1;
+        return 0;
+    }
+    
+    if(dp[n][target] != -1) return dp[n][target];
+    if(arr[n-1] > target) return dp[n][target] = subsetSum(arr, n-1, target, dp);
+    return dp[n][target] = (subsetSum(arr, n-1, target, dp)%mod + subsetSum(arr, n-1, target-arr[n-1], dp)%mod)%mod;
 }
