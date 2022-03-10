d0c8048f1029dfac921077111522a32a6d2c1ab7
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-03-10 06:00:14 +0530

    Update countSubsetSum.cpp

--- DynamicProgramming/countSubsetSum.cpp
+++ DynamicProgramming/countSubsetSum.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//if the input array contains 0 as well
+//then the result will be 
+//let n be the total no. of zeros present
+//result = poy(2, n) * ans;
+
 int countSum(vector<int> &nums, int n, int target, vector<vector<int>> &dp) {
     if(target==0) return 1;
     if(dp[n][target] != -1) return dp[n][target];
@@ -19,5 +24,5 @@ int findWays(vector<int> &nums, int tar)
     
     int n = nums.size();
     vector<vector<int>> dp(n+1, vector<int>(tar+1, -1));
-	return countSum(nums, n, tar, dp);
+    return countSum(nums, n, tar, dp);
 }
