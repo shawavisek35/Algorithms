694132d9e51d286ef56bf4dc2bed48229a094bcb
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-03-14 20:53:51 +0530

    Update coinChangeWithInfiniteSupply.cpp

--- DynamicProgramming/coinChangeWithInfiniteSupply.cpp
+++ DynamicProgramming/coinChangeWithInfiniteSupply.cpp
@@ -15,7 +15,19 @@ public:
     
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount + 1, -1));
-        return countWays(coins, n-1, amount, dp);
+        vector<vector<int>> dp(n+1, vector<int>(amount + 1, 0));
+        
+        //initialization
+        for(int j=0;j<=amount;j++) {
+            if(j%coins[0] == 0) dp[0][j] = 1;
+        }
+        
+        for(int i=1;i<n;i++) {
+            for(int j=0;j<=amount;j++) {
+                if(coins[i] > j) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][j] + dp[i][j-coins[i]];
+            }
+        }
+        return dp[n-1][amount];
     }
 };
