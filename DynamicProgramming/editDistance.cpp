7cfa53ec5ae223ba69d2fe217d86030bbf812930
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-03-19 23:47:07 +0530

    Update editDistance.cpp

--- DynamicProgramming/editDistance.cpp
+++ DynamicProgramming/editDistance.cpp
@@ -26,6 +26,18 @@ public:
         int n = word1.size();
         int m = word2.size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return findMin(word1, word2, n-1, m-1, dp);
+        
+        for(int i=0;i<=n;i++) dp[i][0] = i;
+        for(int j=0;j<=m;j++) dp[0][j] = j;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else {
+                    dp[i][j] = 1 + min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+                }
+            }
+        }
+        //return findMin(word1, word2, n-1, m-1, dp);
+        return dp[n][m];
     }
 };
