2efaf85e03d6f1425972f441d00e79e5ed4eff86
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-03-22 06:52:25 +0530

    Update bestTimeToBuyAndSellStock2.cpp

--- DynamicProgramming/bestTimeToBuyAndSellStock2.cpp
+++ DynamicProgramming/bestTimeToBuyAndSellStock2.cpp
@@ -18,3 +18,25 @@ public:
         return maxProfit;
     }
 };
+
+//alternative way
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int bd = 0;
+        int sd = 0;
+        int profit = 0;
+        int n = prices.size();
+        for(int i=1;i<n;i++) {
+            if(prices[i] >= prices[i-1]) {
+                sd ++;
+            }
+            else {
+                profit += prices[sd] - prices[bd];
+                bd = sd = i;
+            }
+        }
+        profit += prices[sd] - prices[bd];
+        return profit;
+    }
+};
