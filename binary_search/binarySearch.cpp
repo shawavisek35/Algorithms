aab6fc63d463d95d54f0325c77f9cd3a16ccebbb
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-04-01 12:33:26 +0530

    Create binarySearch.cpp

---
+++ binary_search/binarySearch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        while(low <= high) {
+            int mid = (high-low)/2 + low;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[mid] > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
