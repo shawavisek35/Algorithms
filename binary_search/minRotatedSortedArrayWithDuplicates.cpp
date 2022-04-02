79b3518fabe618326dc03c211891b6fa3fc15ae6
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-04-02 19:31:09 +0530

    Create minRotatedSortedArrayWithDuplicates.cpp

---
+++ binary_search/minRotatedSortedArrayWithDuplicates.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = n - 1;
+        int res = INT_MAX;
+        while(i <= j) {
+            int mid = i + (j-i)/2;
+            if(nums[mid] > nums[j]) {
+                i = mid + 1;
+            }
+            else if(nums[mid] < nums[j]) {
+                res = min(res, nums[mid]);
+                j = mid - 1;
+            }
+            else {
+                res = min(res, nums[mid]);
+                j--;
+            }
+        }
+        return res;
+    }
+};
