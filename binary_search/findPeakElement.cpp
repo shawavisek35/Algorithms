b7ee1821bba5c8c8b9f8164b649274157e96e55f
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-04-03 05:34:30 +0530

    Update findPeakElement.cpp

--- binary_search/findPeakElement.cpp
+++ binary_search/findPeakElement.cpp
@@ -20,3 +20,21 @@ public:
 /*
   Time Complexity -> O(logn)
 */
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = n-1;
+        while(i<j) {
+            int mid = i + (j-i)/2;
+            if(nums[mid] > nums[mid + 1]) {
+                j = mid;
+            }
+            else {
+                i = mid + 1;
+            }
+        }
+        return j;
+    }
+};
