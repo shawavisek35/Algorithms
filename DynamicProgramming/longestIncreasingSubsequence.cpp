ea60ce0dd1fcf26d68f8270153829d17d7c45441
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-04-13 19:51:32 +0530

    Update longestIncreasingSubsequence.cpp

--- DynamicProgramming/longestIncreasingSubsequence.cpp
+++ DynamicProgramming/longestIncreasingSubsequence.cpp
@@ -16,4 +16,21 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(n+2, -1));
         return solve(nums, n-1, n, dp);
     }
+    
+    //tabulation
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(2, vector<int> (n+1, 0));
+        for(int i=n-1;i>=0;i--) {
+            for(int j=i-1;j>=-1;j--) {
+                int ans = dp[(i+1)%2][j+1];
+                if(j == -1 || nums[i] > nums[j]) {
+                    ans = max(ans, 1 + dp[(i+1)%2][i+1]);
+                }
+
+                dp[i%2][j+1] = ans;
+            }
+        }
+        return dp[0][0];
+    }
 };
